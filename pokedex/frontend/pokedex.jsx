d7ccedd0c2ca79e9f2b25401228fa30e408f3f89
andywynkoop <cheeeya@berkeley.edu>
2018-03-29 17:02:26 -0700

    Day 2 work

--- pokedex/frontend/pokedex.jsx
+++ pokedex/frontend/pokedex.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { receiveAllPokemon, requestAllPokemon } from './actions/pokemon_actions';
-import * as APIUtil from './util/api_util';
 import configureStore from './store/store';
-
-
+import Root from './components/root';
+import { HashRouter, Route } from 'react-router-dom';
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
   const store = configureStore();
-
-  window.receiveAllPokemon = receiveAllPokemon;
-  window.fetchAllPokemon = APIUtil.fetchAllPokemon;
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  window.requestAllPokemon = requestAllPokemon;
-
-  ReactDOM.render(<h1>Pokedex</h1>, rootEl);
+  window.store = store;
+  ReactDOM.render(<Root store={store}/>, rootEl);
 });
